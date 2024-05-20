275539d7b232d6cc7b0c0ae7f87ec9ee136ce47d
AmiraBahaa <amirabahaa945@yahoo.com>
2024-05-20 18:54:40 +0300

    update data file

--- src/test/java/tests/UserRegisterationTest.java
+++ src/test/java/tests/UserRegisterationTest.java
@@ -1,14 +1,16 @@
 package tests;
 
+import data.ExcelReader;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.HomePage;
-import pages.LoginPage;
 import pages.UserRegisterationPage;
 
+import java.io.IOException;
 import java.time.Duration;
 
 public class UserRegisterationTest extends TestBase{
@@ -17,13 +19,19 @@ public class UserRegisterationTest extends TestBase{
     UserRegisterationPage userRegisterationPageObject;
 
 
+    @DataProvider(name = "ExcelData")
+    public Object [][] userRegisterData() throws IOException {
+        ExcelReader excelReader= new ExcelReader();
+        return excelReader.getExcelData();
 
-    @Test
-    public void UserSuccessfullyRegistered(){
+    }
+
+    @Test(priority = 1, alwaysRun = true, dataProvider = "ExcelData")
+    public void UserSuccessfullyRegistered(String firstName , String lastName , String email , String password){
        homePage = new HomePage(driver);
        homePage.openRegisterPage();
        userRegisterationPageObject = new UserRegisterationPage(driver);
-       userRegisterationPageObject.registeration("Amira", "Bahaa", "amirabahaa7@gmail.com", "185cmcmc");
+       userRegisterationPageObject.registeration(firstName, lastName, email, password);
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
         WebElement registerationMessageReady = wait.until(ExpectedConditions.visibilityOf(userRegisterationPageObject.registerationMessage));
         Assert.assertTrue(registerationMessageReady.getText().contains("Your registration completed"));
